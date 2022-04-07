e28202af0c7272010d4fb5d73e57edcd9e7db2e9
Pierrii <99651519+Pierrii@users.noreply.github.com>
2022-04-07 09:29:47 +0200

    test

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
 
-if(isset($_POST['areYouSure'])) {
-    // laat de "are you sure?" zien. //: ja± IAmSure, userID
-} else if($_POST['IAmSure']) {
-    // verwerken actie
-}else {
-    // Laat het tabel zien. AreYouSure, userID
-}
+// if(isset($_POST['areYouSure'])) {
+//     // laat de "are you sure?" zien. //: ja± IAmSure, userID
+// } else if($_POST['IAmSure']) {
+//     // verwerken actie
+// }else {
+//     // Laat het tabel zien. AreYouSure, userID
+// }
+
+var_dump($_POST);
